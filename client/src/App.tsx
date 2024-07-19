4a0a788a9798cf94ac1686074fb2323005bf8069
woobuntu <woobuntu2022@gmail.com>
2024-07-19 14:42:54 +0900

    글로벌 폰트를 MaterialUiProvider 컴포넌트로 이동

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,11 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { MaterialUiProviders } from "./providers/MaterialUiProviders";
 import { Layout } from "./layouts/Layout";
 
 function App() {
   return (
-    <>
-      <MaterialUiProviders>
-        <Layout></Layout>
-      </MaterialUiProviders>
-    </>
+    <MaterialUiProviders>
+      <Layout></Layout>
+    </MaterialUiProviders>
   );
 }
 
