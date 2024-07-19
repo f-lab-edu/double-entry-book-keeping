d02bf68cf73de9bdd205a03086221dbcb3299ba8
woobuntu <woobuntu2022@gmail.com>
2024-07-19 14:40:51 +0900

    앱 자체에 대한 레이아웃 컴포넌트 작성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,14 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { MaterialUiProviders } from "./providers/MaterialUiProviders";
+import { Layout } from "./layouts/Layout";
 
 function App() {
   return (
     <>
-      <MaterialUiProviders></MaterialUiProviders>
+      <MaterialUiProviders>
+        <Layout></Layout>
+      </MaterialUiProviders>
     </>
   );
 }
