ba41363ed69e49232b3a87e0703158071f7aa1ec
woobuntu <woobuntu2022@gmail.com>
2024-07-19 21:57:47 +0900

    react-query 기본 설정

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -1,13 +1,16 @@
 import { MaterialUiProviders } from "./providers/MaterialUiProviders";
 import { Layout } from "./layouts/Layout";
 import { Outlet } from "react-router-dom";
+import { ReactQueryProvider } from "./providers/ReactQueryProvider";
 
 function Root() {
   return (
     <MaterialUiProviders>
-      <Layout>
-        <Outlet />
-      </Layout>
+      <ReactQueryProvider>
+        <Layout>
+          <Outlet />
+        </Layout>
+      </ReactQueryProvider>
     </MaterialUiProviders>
   );
 }
