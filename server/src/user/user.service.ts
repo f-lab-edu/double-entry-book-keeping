c11b6c27fa3f71dcacd342c99fa80f77d9178d2e
woobuntu <woobuntu2022@gmail.com>
2024-07-29 12:59:04 +0900

    중복된 아이디로 가입하려고 할 때의 예외처리

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
@@ -7,13 +7,19 @@ export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(id: string, hashedPassword: string, salt: string) {
-    return this.prisma.user.create({
-      data: {
-        id,
-        password: hashedPassword,
-        salt,
-      },
-    });
+    const user = await this.read(id);
+
+    if (user) {
+      throw new ConflictException('해당 아이디의 사용자가 이미 존재합니다.');
+    } else {
+      return this.prisma.user.create({
+        data: {
+          id,
+          password: hashedPassword,
+          salt,
+        },
+      });
+    }
   }
 
   async read(id: Prisma.UserWhereUniqueInput['id']) {
