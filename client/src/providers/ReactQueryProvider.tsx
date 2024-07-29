eef18fdb4ffa67a23cee4f141cae9352c70885d9
woobuntu <woobuntu2022@gmail.com>
2024-07-29 12:44:08 +0900

    회원가입 및 로그인 페이지 외에는 모두 인증이 필요한 페이지
    - axiox의 인터셉터에서 훅 사용이 필요한 부분이 있어, axios와 연동하여 사용하는 react-query와 연관된 ReactQueryProvider에 작성

--- client/src/providers/ReactQueryProvider.tsx
+++ client/src/providers/ReactQueryProvider.tsx
@@ -1,10 +1,30 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { useLocation, useNavigate } from "react-router-dom";
+import { axiosInstance } from "../axios-instance";
 
 const queryClient = new QueryClient();
 
 export const ReactQueryProvider = ({ children }: PropsWithChildren) => {
+  const navigate = useNavigate();
+
+  const location = useLocation();
+
+  axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (
+        location.pathname !== "/sign-in" &&
+        location.pathname !== "/sign-up" &&
+        error.response.status == 401
+      ) {
+        navigate("/sign-in");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
