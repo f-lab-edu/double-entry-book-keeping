921d8488eee4afa3f823bf1cbe25c1a5ad3478cf
woobuntu <woobuntu2022@gmail.com>
2024-08-02 21:32:26 +0900

    jwt 토큰의 만료시간과 쿠키의 만료 시간은 별개이므로 쿠키의 만료시간도 설정해 주어야 한다.

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -30,6 +30,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
+      maxAge: 60 * 1000,
     });
 
     return { message };
@@ -47,6 +48,7 @@ export class AuthController {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'strict',
+      maxAge: 60 * 1000,
     });
 
     return { message };
