bbdd69be1f531365c48951f7ebcef5c8b1d39265
woobuntu <woobuntu2022@gmail.com>
2024-09-12 16:40:46 +0900

    healthz가 여러 쿠버네티스 컴포넌트에서 사용하는 표준 경로이기 때문에 경로 수정 + storage때문에 헬스 체크 통과 못해서 수정

--- server/src/health/health.controller.ts
+++ server/src/health/health.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/terminus';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Controller('health')
+@Controller('healthz')
 export class HealthController {
   constructor(
     private readonly health: HealthCheckService,
@@ -18,7 +18,7 @@ export class HealthController {
     private readonly prisma: PrismaService,
     private readonly disk: DiskHealthIndicator,
     private readonly memory: MemoryHealthIndicator,
-  ) {}
+  ) { }
 
   @Get()
   @HealthCheck()
@@ -27,7 +27,7 @@ export class HealthController {
       () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
       () => this.db.pingCheck('prisma', this.prisma),
       () =>
-        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.8 }),
       () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
     ]);
   }
